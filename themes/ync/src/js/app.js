5be9aa457d72c8ae305a461b2863ad3153706d3c
Nirajan Basnet <er.nirajan.basnet@gmail.com>
2019-06-11 14:30:57 +0545

    Banner section UI

---
+++ themes/ync/src/js/app.js
@@ -0,0 +1,10 @@
+const bg = document.querySelector('.confetti');
+const windowWidth = window.innerWidth / 5;
+const windowHeight = window.innerHeight / 5 ;
+
+bg.addEventListener('mousemove', (e) => {
+  const mouseX = e.clientX / windowWidth;
+  const mouseY = e.clientY / windowHeight;
+  
+  bg.style.transform = `translate3d(-${mouseX}%, -${mouseY}%, 0)`;
+});
--- themes/ync/src/js/tailwind.config.js
+++ themes/ync/src/js/tailwind.config.js
@@ -1,7 +1,42 @@
+const { colors } = require('tailwindcss/defaultTheme')
+
 module.exports = {
   theme: {
-    extend: {}
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+    },
+    fontFamily: {
+      quicksand: ['Quicksand', 'sans-serif'],
+      body: ['Lato', 'sans-serif'],
+    },
+    textColor: {
+      'base-color': '#4A4A4A',
+      black: '#000000',
+      primary: '#7E1BF4',
+      secondary: '#F67534'
+    },
+    colors: {
+    
+    },
+    borderWidth: {
+      default: '1px',
+      '0': '0',
+      '2': '2px',
+      '4': '4px',
+    },
+    extend: {
+      colors: {
+        cyan: '#9cdbff',
+      },
+      spacing: {
+        '96': '24rem',
+        '128': '32rem',
+      }
+    }
   },
   variants: {},
   plugins: []
-}
+};
